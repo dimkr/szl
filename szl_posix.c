091c9c8cf3c9b56f2fa6276005159294b180c990
Dima Krasner <dima@dimakrasner.com>
2016-04-26 21:50:35 +0300

    Added exec

--- szl_posix.c
+++ szl_posix.c
@@ -26,9 +26,14 @@
 #include <time.h>
 #include <math.h>
 #include <errno.h>
-#include <dlfcn.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <paths.h>
 
 #include "szl.h"
+
+#define EXEC_BUFSIZ 4096
 
 static enum szl_res szl_posix_proc_sleep(struct szl_interp *interp,
                                          const int objc,
@@ -59,16 +64,110 @@ static enum szl_res szl_posix_proc_sleep(struct szl_interp *interp,
 	return SZL_OK;
 }
 
+static enum szl_res szl_posix_proc_exec(struct szl_interp *interp,
+                                        const int objc,
+                                        struct szl_obj **objv,
+                                        struct szl_obj **ret)
+{
+	char *buf, *mbuf;
+	const char *cmd;
+	size_t len, mlen, rlen;
+	pid_t pid;
+	int fds[2], out, status;
+	ssize_t clen;
+	enum szl_res res = SZL_OK;
+
+	cmd = szl_obj_str(objv[1], &len);
+	if (!cmd || !len)
+		return SZL_ERR;
+
+	if (pipe(fds) < 0)
+		return SZL_ERR;
+
+	pid = fork();
+	switch (pid) {
+		case -1:
+			close(fds[1]);
+			close(fds[0]);
+			return SZL_ERR;
+
+		case 0:
+			close(fds[0]);
+			out = dup2(fds[1], STDOUT_FILENO);
+			close(fds[1]);
+			if (out == STDOUT_FILENO)
+				execl(_PATH_BSHELL, _PATH_BSHELL, "-c", cmd, (char *)NULL);
+			exit(EXIT_FAILURE);
+	}
+
+	close(fds[1]);
+
+	buf = (char *)malloc(EXEC_BUFSIZ + 1);
+	if (!buf) {
+		close(fds[0]);
+		return SZL_ERR;
+	}
+
+	len = mlen = EXEC_BUFSIZ;
+	rlen = 0;
+	do {
+		clen = read(fds[0], buf + rlen, EXEC_BUFSIZ);
+		if (clen < 0) {
+			res = SZL_ERR;
+			break;
+		}
+		if (clen == 0)
+			break;
+
+		mlen = len + EXEC_BUFSIZ;
+		mbuf = (char *)realloc(buf, mlen);
+		if (!mbuf) {
+			free(buf);
+			res = SZL_ERR;
+			break;
+		}
+		buf = mbuf;
+		len = mlen;
+
+		rlen += clen;
+	} while (rlen <= SIZE_MAX);
+
+	close(fds[0]);
+
+	if (waitpid(pid, &status, 0) != pid) {
+		free(buf);
+		return SZL_ERR;
+	}
+
+	if (res == SZL_OK) {
+		buf[rlen] = '\0';
+		*ret = szl_new_str_noalloc(buf, rlen);
+		if (*ret)
+			return SZL_OK;
+	}
+
+	free(buf);
+	return res;
+}
+
 enum szl_res szl_init_posix(struct szl_interp *interp)
 {
-	if (!szl_new_proc(interp,
-	                  "sleep",
-	                  2,
-	                  2,
-	                  "sleep sec",
-	                  szl_posix_proc_sleep,
-	                  NULL,
-	                  NULL))
+	if ((!szl_new_proc(interp,
+	                   "sleep",
+	                   2,
+	                   2,
+	                   "sleep sec",
+	                   szl_posix_proc_sleep,
+	                   NULL,
+	                   NULL)) ||
+	    (!szl_new_proc(interp,
+	                   "exec",
+	                   2,
+	                   2,
+	                   "exec cmd",
+	                   szl_posix_proc_exec,
+	                   NULL,
+	                   NULL)))
 		return SZL_ERR;
 
 	return SZL_OK;
